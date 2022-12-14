36ae1e7f774716aefb0d2a09b9c72f63ef21dde3
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-12-14 15:19:27 -0300

    :recycle: refactor: funções movidas e melhora no localStorage
    main.js agora somente pega o botão e adiciona o eventListener; nova forma de adicionar os dados ao localStorage.

---
+++ assets/js/components/criaTarefa.js
@@ -0,0 +1,37 @@
+import BotaoConclui from "./concluiTarefa.js"
+import BotaoDeleta from "./deletaTarefa.js"
+
+export const handleNovoItem = (evento) => {
+   evento.preventDefault();
+   const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+   const input = document.querySelector('[data-form-input]');
+   const valor = input.value;
+   const calendario = document.querySelector('[data-form-date]');
+   const data = moment(calendario.value);
+   const dataFormatada = data.format('DD/MM/YYYY');
+   const lista = document.querySelector('[data-list]');
+   const dados = {
+      dataFormatada,
+      valor
+   }
+   const criaTarefa = Tarefa(dados);
+
+   const tarefasAtualizadas = { ...tarefas, dados };
+
+   localStorage.setItem("tarefas", JSON.stringify(tarefasAtualizadas));
+
+   lista.appendChild(criaTarefa);
+   input.value = '';
+}
+
+const Tarefa = ({ dataFormatada, valor }) => {
+   const tarefa = document.createElement('li');
+   tarefa.classList.add('task');
+   const conteudo = `<p class=content>${dataFormatada} * ${valor}</p>`
+   tarefa.innerHTML = conteudo;
+   const span = document.createElement('span');
+   span.appendChild(BotaoConclui());
+   span.appendChild(BotaoDeleta());
+   tarefa.appendChild(span);
+   return tarefa;
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,4 @@
-import BotaoConclui from "./components/concluiTarefa.js"
-import BotaoDeleta from "./components/deletaTarefa.js"
-
-const tarefas = [];
-const handleNovoItem = (evento) => {
-   evento.preventDefault();
-   const input = document.querySelector('[data-form-input]');
-   const valor = input.value;
-   const calendario = document.querySelector('[data-form-date]');
-   const data = moment(calendario.value);
-   const dataFormatada = data.format('DD/MM/YYYY');
-   const lista = document.querySelector('[data-list]');
-   const dados = {
-      dataFormatada,
-      valor
-   }
-   const criaTarefa = criarTarefa(dados);
-
-   tarefas.push(dados);
-   localStorage.setItem("tarefas", JSON.stringify(tarefas));
-
-   lista.appendChild(criaTarefa);
-   input.value = '';
-}
-
-const criarTarefa = ({ dataFormatada, valor }) => {
-   const tarefa = document.createElement('li');
-   tarefa.classList.add('task');
-   const conteudo = `<p class=content>${dataFormatada} * ${valor}</p>`
-   tarefa.innerHTML = conteudo;
-   const span = document.createElement('span');
-   span.appendChild(BotaoConclui());
-   span.appendChild(BotaoDeleta());
-   tarefa.appendChild(span);
-   return tarefa;
-}
+import { handleNovoItem } from "./components/criaTarefa.js";
 
 const novaTarefa = document.querySelector('[data-form-button]');
 
