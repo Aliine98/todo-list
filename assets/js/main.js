846f3014ca481a480ad0e22f1d8ed6a0a218c951
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-12-14 14:06:02 -0300

    :sparkles: feat: exibindo data na tarefa
    O valor da data foi formatado usando moment.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,13 @@ const criarTarefa = (evento) => {
    evento.preventDefault();
    const input = document.querySelector('[data-form-input]');
    const valor = input.value;
+   const calendario = document.querySelector('[data-form-date]');
+   const data = moment(calendario.value);
+   const dataFormatada = data.format('DD/MM/YYYY');
    const lista = document.querySelector('[data-list]');
    const tarefa = document.createElement('li');
    tarefa.classList.add('task');
-   const conteudo = `<p class=content>${valor}</p>`
+   const conteudo = `<p class=content>${dataFormatada} * ${valor}</p>`
    tarefa.innerHTML = conteudo;
    const span = document.createElement('span');
    span.appendChild(BotaoConclui());
