ece137ff1749bac96d9b3e11cf9cf9d845b06199
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-12-15 13:19:07 -0300

    :sparkles: feat: nova função ordenaDatas
    utilizando método sort no array de datas únicas, função sendo chamada no arquivo carregaTarefa.js

--- assets/js/service/data.js
+++ assets/js/service/data.js
@@ -5,3 +5,11 @@ export const removeDatasRepetidas = (datas) => {
    });
    return Array.from(datasUnicas);
 }
+
+export const ordenaDatas = (datas) => {
+   datas.sort((a, b) => {
+      const primeiraData = moment(a, 'DD/MM/YYYY').format('YYYYMMDD');
+      const segundaData = moment(b, 'DD/MM/YYYY').format('YYYYMMDD');
+      return primeiraData - segundaData;
+   });
+}
